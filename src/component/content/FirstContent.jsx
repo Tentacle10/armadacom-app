33f8276e87f201925d7ff4a952f4642c78593c54
4gungriyant0 <4gungriyant0@gmail.com>
2023-10-11 23:09:24 +0700

    navbarv1+contentv1

--- src/component/content/FirstContent.jsx
+++ src/component/content/FirstContent.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
 import './content.css';
 
 import CrossPlatform from '../../assets/img/Cross-platform.png';
@@ -9,44 +10,53 @@ import CPUBlack from '../../assets/img/cpublack.png';
 const FirstContent = () => {
   return (
     <>
-      <Container fluid>
+      <Container className="slideshow" fluid>
         <Carousel className="carousel pt-5" controls={false} indicators={true} fade>
           <Carousel.Item>
             <Row>
-              <Col sm={6} className="imgslide">
+              <Col sm={8} className="imgslide">
                 <img src={CrossPlatform} alt="slide" text="First slide" />
               </Col>
-              <Col sm={6}>
+              <Col sm={4}>
                 <Carousel.Caption>
-                  <h3>Ga Cuma Servis dan Jualan, Bikin Aplikasi juga.</h3>
+                  <h1>Ga Cuma Servis dan Jualan, Bikin Aplikasi juga.</h1>
                   <p>Bikin aplikasi berbasis web ataupun Android di Wonosobo ya ke armadacom.id. Sebagai penunjang aktifitas bisnis kalian</p>
                 </Carousel.Caption>
+                <Button className="mt-2" variant="outline-light">
+                  CHAT US
+                </Button>
               </Col>
             </Row>
           </Carousel.Item>
           <Carousel.Item>
             <Row>
-              <Col sm={6} className="imgslide">
+              <Col sm={8} className="imgslide">
                 <img src={HPElite} alt="slide" text="Second slide" />
               </Col>
-              <Col sm={6}>
+              <Col sm={4}>
                 <Carousel.Caption>
-                  <h3>Udah muter muter, ujungnya ke Armadacom.id</h3>
+                  <h1>Udah muter muter, ujungnya ke Armadacom.id</h1>
                   <p>Armadacom.id adalah Toko komputer terlengkap di Wonosobo, menyediakan berbagai perangkat komputer dan perangkat jaringan</p>
                 </Carousel.Caption>
+                <Button className="mt-2" variant="outline-light">
+                  CHAT US
+                </Button>
               </Col>
             </Row>
           </Carousel.Item>
           <Carousel.Item>
             <Row>
-              <Col sm={6} className="imgslide">
+              <Col sm={8} className="imgslide">
                 <img src={CPUBlack} alt="slide" text="Third slide" />
               </Col>
-              <Col sm={6}>
+              <Col sm={4}>
                 <Carousel.Caption>
-                  <h3>Ngapain Berhari hari, Kalo Sehari Jadi</h3>
+                  <h1>Ngapain Berhari hari, Kalo Sehari Jadi</h1>
                   <p>Bikin aplikasi berbasis web ataupun Android di Wonosobo ya ke armadacom.id. Sebagai penunjang aktifitas bisnis kalian</p>
                 </Carousel.Caption>
+                <Button className="mt-2" variant="outline-light">
+                  CHAT US
+                </Button>
               </Col>
             </Row>
           </Carousel.Item>
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 
 import Container from 'react-bootstrap/Container';
@@ -10,11 +10,22 @@ import Button from 'react-bootstrap/Button';
 import ArmadaIcon from '../../assets/logo/Logo_white.png';
 
 const Header = () => {
+  useEffect(() => {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+        navbar.classList.add('sticky-navbar');
+      } else {
+        navbar.classList.remove('sticky-navbar');
+      }
+    });
+  }, []);
   return (
     <>
-      <Navbar expand="md" className="bg-body-tertiary" fixed="top">
+      <Navbar expand="md" className="navbar">
         <Container fluid>
-          <Navbar className="bg-body-tertiary">
+          <Navbar>
             <Container>
               <Navbar.Brand href="#home">
                 <img src={ArmadaIcon} width="90" height="30" className="d-inline-block align-top" alt="Logo_Armada" />
@@ -25,14 +36,14 @@ const Header = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#home">HOME</Nav.Link>
-              <Nav.Link href="#link">ABOUT US</Nav.Link>
+              <Nav.Link href="#about">ABOUT US</Nav.Link>
               <NavDropdown title="LAYANAN" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">STORE</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">SERVICE</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">SOFTWARE</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#link">PRICELIST</Nav.Link>
-              <Nav.Link href="#link">TESTIMONIAL</Nav.Link>
+              <Nav.Link href="#pricelist">PRICELIST</Nav.Link>
+              <Nav.Link href="#testimonial">TESTIMONIAL</Nav.Link>
             </Nav>
             <Button className="ms-4 me-2" variant="outline-light">
               GET APP
