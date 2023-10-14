f08965b1e323edd2af973371df233ec230c4a842
unknown <siriusz2@gmail.com>
2023-10-14 16:22:56 +0700

    contentv3

--- src/component/content/SecondContent.jsx
+++ src/component/content/SecondContent.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
-import HandymanRoundedIcon from '@mui/icons-material/HandymanRounded';
-import TerminalRoundedIcon from '@mui/icons-material/TerminalRounded';
-import SupportAgentRoundedIcon from '@mui/icons-material/SupportAgentRounded';
-import { faAlignJustify } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import StoreRoundedIcon from "@mui/icons-material/StoreRounded";
+import HandymanRoundedIcon from "@mui/icons-material/HandymanRounded";
+import TerminalRoundedIcon from "@mui/icons-material/TerminalRounded";
+import SupportAgentRoundedIcon from "@mui/icons-material/SupportAgentRounded";
 
 const SecondContent = () => {
   return (
@@ -16,9 +15,14 @@ const SecondContent = () => {
             <Col>
               <Col>
                 <Col className="tag">ARMADACOM.ID</Col>
-                <Col className="title fw-bolder">Melayani segala kebutuhan IT</Col>
+                <Col className="title fw-bolder">
+                  Melayani segala kebutuhan IT
+                </Col>
                 <Col className="text">
-                  Armada Computer atau yang sekarang lebih dikenal dengan Armadacom.id merupakan toko komputer terlengkap di Wonosobo yang menyediakan berbagai peripheral komputer dan jaringan. Berikut layanan yang kami sediakan
+                  Armada Computer atau yang sekarang lebih dikenal dengan
+                  Armadacom.id merupakan toko komputer terlengkap di Wonosobo
+                  yang menyediakan berbagai peripheral komputer dan jaringan.
+                  Berikut layanan yang kami sediakan
                 </Col>
               </Col>
             </Col>
@@ -29,22 +33,30 @@ const SecondContent = () => {
                 <Col md={6}>
                   <Card style={{}}>
                     <div sm={6} className="icon text-center">
-                      <StoreRoundedIcon sx={{ fontSize: 50 }} />
+                      <StoreRoundedIcon
+                        sx={{ fontSize: 50, color: "#70707e" }}
+                      />
                     </div>
                     <Card.Body>
                       <Card.Title>Store</Card.Title>
-                      <Card.Text>Penjualan Perangkat Hardware dan Software</Card.Text>
+                      <Card.Text>
+                        Penjualan Perangkat Hardware dan Software
+                      </Card.Text>
                     </Card.Body>
                   </Card>
                 </Col>
                 <Col md={6}>
                   <Card style={{}}>
                     <div className="text-center">
-                      <HandymanRoundedIcon sx={{ fontSize: 50 }} />
+                      <HandymanRoundedIcon
+                        sx={{ fontSize: 50, color: "#70707e" }}
+                      />
                     </div>
                     <Card.Body>
                       <Card.Title>Service Center</Card.Title>
-                      <Card.Text>Service perangkat Laptop, Komputer, Printer, dll.</Card.Text>
+                      <Card.Text>
+                        Service perangkat Laptop, Komputer, Printer, dll.
+                      </Card.Text>
                     </Card.Body>
                   </Card>
                 </Col>
@@ -53,22 +65,30 @@ const SecondContent = () => {
                 <Col md={6}>
                   <Card style={{}}>
                     <div className="text-center">
-                      <TerminalRoundedIcon sx={{ fontSize: 50 }} />
+                      <TerminalRoundedIcon
+                        sx={{ fontSize: 50, color: "#70707e" }}
+                      />
                     </div>
                     <Card.Body>
                       <Card.Title>Software Dev</Card.Title>
-                      <Card.Text>Pengembangan software berbasis Web dan Android.</Card.Text>
+                      <Card.Text>
+                        Pengembangan software berbasis Web dan Android.
+                      </Card.Text>
                     </Card.Body>
                   </Card>
                 </Col>
                 <Col md={6}>
                   <Card style={{}}>
                     <div className="text-center">
-                      <SupportAgentRoundedIcon sx={{ fontSize: 50 }} />
+                      <SupportAgentRoundedIcon
+                        sx={{ fontSize: 50, color: "#70707e" }}
+                      />
                     </div>
                     <Card.Body>
                       <Card.Title>IT Consultant</Card.Title>
-                      <Card.Text>Konsultasi terkait kebutuhan dab produk IT.</Card.Text>
+                      <Card.Text>
+                        Konsultasi terkait kebutuhan dab produk IT.
+                      </Card.Text>
                     </Card.Body>
                   </Card>
                 </Col>
--- src/component/content/ThirdContent.jsx
+++ src/component/content/ThirdContent.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import iPhone from "../../assets/img/Iphone-Mockup.png";
+import Button from "react-bootstrap/Button";
+import PlayStore from "../../assets/icon/playstore-svgrepo-com.svg";
 
 const ThirdContent = () => {
   return (
@@ -7,13 +10,51 @@ const ThirdContent = () => {
       <Container
         style={{
           width: "100%",
-          height: "100vh",
+          height: "100%",
           backgroundColor: "#2e64aa",
           marginTop: "3rem",
           borderRadius: "20px",
         }}
+        className="thirdcontent d-flex justify-content-center"
       >
-        <div></div>
+        <Row className="d-flex justify-content-center">
+          <Col
+            md={4}
+            style={{
+              backgroundColor: "#fff",
+              height: "100%",
+              borderRadius: "20px",
+              justifyContent: "center",
+            }}
+            className="iPhone p-3"
+          >
+            <img src={iPhone} alt="iPhone" style={{ width: "100%" }} />
+          </Col>
+          <Col md={8} className="text-light">
+            <Row className="d-flex">
+              <Row md={12} className="txt-center fw-bold">
+                <div>Fitur Tracking Service</div>
+              </Row>
+              <Row md={12} className="fw-lighter">
+                Memudahkan customer dalam memantau progres service
+              </Row>
+              <Row md={12} className="fw-bold">
+                Aplikasi Armadacom
+              </Row>
+              <Row md={12} className="fw-lighter">
+                Download sekarang di playstore untuk menikmati fitur tracking
+                dan live update harga.
+              </Row>
+              <Button
+                className="ms-4 me-2 w-button d-flex align-items-center px-3 py-2"
+                variant="outline-light"
+              >
+                <img src={PlayStore} alt="PlayStore" width={20} />
+                Download
+              </Button>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </>
   );
