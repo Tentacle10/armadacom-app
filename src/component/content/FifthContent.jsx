8bec8e8630e7513ac1de1154836d9b5bb55a2679
unknown <siriusz2@gmail.com>
2023-10-18 16:54:27 +0700

    contentV5rev2

--- src/component/content/FifthContent.jsx
+++ src/component/content/FifthContent.jsx
@@ -1,34 +1,56 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import Devimg from '../../assets/img/devimg.jpg';
-import Toska from '../../assets/img/dev/toska.png';
+import { Container, Row, Col } from "react-bootstrap";
+import Devimg from "../../assets/img/devimg.jpg";
+import Card from "../card/Card.jsx";
+import Toska from "../../assets/img/dev/toska.png";
+import UpSensei from "../../assets/img/dev/upsensei.png";
+import evoting from "../../assets/img/dev/evoting.png";
+import custom from "../../assets/img/dev/custom.png";
 
 const FifthContent = () => {
   return (
     <>
-      <Container className='mt-4'>
+      <Container className="mt-4">
         <Row>
-          <Col sm={6} className='mb-3'>
-            <img src={Devimg} alt='dev' className='dev-img w-100' />
+          <Col sm={6} className="mb-3">
+            <img src={Devimg} alt="dev" className="dev-img w-100" />
           </Col>
           <Col sm={6}>
-            <div className='fs-4 fw-bolder'>Software House</div>
-            <div className='fs-2 fw-bolder'>PALUGADA - Apa yang lu mau, Gue ada!</div>
-            <div className='fs-6 text-abu'>Menyediakan software untuk penunjang aktifitas bisnis ataupun kegiatan instansi, dengan teknologi sebagai alat untuk meningkatkan efektifitas, kecepatan dan ketepatan dalam bekerja.</div>
+            <div className="fs-4 fw-bolder">Software House</div>
+            <div className="fs-2 fw-bolder">
+              PALUGADA - Apa yang lu mau, Gue ada!
+            </div>
+            <div className="fs-6 text-abu">
+              Menyediakan software untuk penunjang aktifitas bisnis ataupun
+              kegiatan instansi, dengan teknologi sebagai alat untuk
+              meningkatkan efektifitas, kecepatan dan ketepatan dalam bekerja.
+            </div>
           </Col>
           <Col>
-            <Card>
-              <Row>
-                <Col>
-                  <img src={Toska} alt='Devimg' className='w-25' />
-                  <Card.Body>
-                    <Card.Title>TOSKA - Aplikasi Kasir</Card.Title>
-                    <Card.Text className='text-abu'>Aplikasi kasir berbasis website yang bisa kalian pakai dari mana aja dengan fitur lengkap dan interface yang memukau.</Card.Text>
-                  </Card.Body>
-                </Col>
-              </Row>
-            </Card>
+            <Card
+              title="Aplikasi Kasir"
+              img={Toska}
+              desc="Aplikasi kasir berbasis website yang bisa kalian pakai dari mana
+              aja dengan fitur lengkap dan interface yang memukau."
+              harga="1.499K"
+            />
+            <Card
+              title="UPSENSEI - Absen App"
+              img={UpSensei}
+              desc="menggunakan ID Card sebagai device absen, dengan laporan yang presisi dan mudah dipahami. Cepat,Mudah,Akurat."
+              harga="1.499K"
+            />
+            <Card
+              title="EVoting - Vote App"
+              img={evoting}
+              desc="Software vote pemilihan pimpinan untuk organisasi maupun instansi, kemudahan dalam rekap dan tanpa media kertas."
+              harga="2.250K"
+            />
+            <Card
+              title="Custom Software"
+              img={custom}
+              desc="Melayani pembuatan software berbasis website maupun android by request sesuai kebutuhan dan keinginan kalian."
+              harga="$$$$$"
+            />
           </Col>
         </Row>
       </Container>
