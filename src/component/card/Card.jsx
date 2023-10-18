46aa612b29c0a63269ea9f73238e05c17f55c169
4gungriyant0 <4gungriyant0@gmail.com>
2023-10-18 23:58:28 +0700

    navbarMobileV1

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { Row, Col, Collapse, Card } from "react-bootstrap";
-import { CaretDownFill, CheckCircleFill, CheckLg } from "react-bootstrap-icons";
-import { PropTypes } from "prop-types";
+import { useState } from 'react';
+import { Row, Col, Collapse, Card } from 'react-bootstrap';
+import { CaretDownFill, CheckLg } from 'react-bootstrap-icons';
+import { PropTypes } from 'prop-types';
 
 const CardDev = (props) => {
   const { title, img, desc, harga } = props;
@@ -11,57 +11,52 @@ const CardDev = (props) => {
   };
 
   return (
-    <Card
-      className="mt-3"
-      onClick={handleButtonClick}
-      aria-controls="example-collapse-text"
-      aria-expanded={open}
-    >
+    <Card className='mt-3' onClick={handleButtonClick} aria-controls='example-collapse-text' aria-expanded={open}>
       <Row>
         <Col>
-          <Card.Title className="d-flex align-items-center mt-2">
-            <img src={img} alt="Devimg" className="logodev ms-3 me-3" />
+          <Card.Title className='d-flex align-items-center mt-2'>
+            <img src={img} alt='Devimg' className='logodev ms-3 me-3' />
             {title}
           </Card.Title>
-          <Card.Body className="ps-3 pt-1">
-            <Card.Text className="text-abu">{desc}</Card.Text>
+          <Card.Body className='ps-3 pt-1'>
+            <Card.Text className='text-abu'>{desc}</Card.Text>
           </Card.Body>
         </Col>
       </Row>
       <Row>
         <Col>
           <Collapse in={open}>
-            <div id="example-collapse-text">
-              <div className="text-center mt-2">
-                <div className="fs-6 text-abu">Start From</div>
-                <div className="harga">{harga}</div>
+            <div id='example-collapse-text'>
+              <div className='text-center mt-2'>
+                <div className='fs-6 text-abu'>Start From</div>
+                <div className='harga'>{harga}</div>
               </div>
-              <div className="fitur px-4 py-4">
+              <div className='fitur px-4 py-4'>
                 <p>
-                  <CheckLg className="check me-4" />
+                  <CheckLg className='check me-4' />
                   Cross Platform
                 </p>
                 <p>
-                  <CheckLg className="check me-4" />2 in 1 POS Jasa & Barang
+                  <CheckLg className='check me-4' />2 in 1 POS Jasa & Barang
                 </p>
                 <p>
-                  <CheckLg className="check me-4" />
+                  <CheckLg className='check me-4' />
                   Laporan Transaksi
                 </p>
                 <p>
-                  <CheckLg className="check me-4" />
+                  <CheckLg className='check me-4' />
                   Device Printing
                 </p>
                 <p>
-                  <CheckLg className="check me-4" />
+                  <CheckLg className='check me-4' />
                   Setting Lengkap & Mudah
                 </p>
               </div>
             </div>
           </Collapse>
         </Col>
-        <div className="trapesium text-center position-absolute bottom-0 start-50 translate-middle-x d-flex justify-content-center text-white">
-          <CaretDownFill className={open && "rotate-icon"} />
+        <div className='trapesium text-center position-absolute bottom-0 start-50 translate-middle-x d-flex justify-content-center text-white'>
+          <CaretDownFill className={open ? 'rotate-icon' : ''} />
         </div>
       </Row>
     </Card>
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,15 +1,23 @@
-// import React, { useEffect } from 'react';
-import "./Header.css";
+// import { useState } from 'react';
+import './Header.css';
 
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Button from "react-bootstrap/Button";
+import { Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-import ArmadaIcon from "../../assets/logo/Logo_white.png";
+import ArmadaIcon from '../../assets/logo/Logo_white.png';
+
+// import { HouseFill, FileEarmarkPersonFill } from 'react-bootstrap-icons';
 
 const Header = () => {
+  // const [show, setShow] = useState(false);
+
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
   // useEffect(() => {
   //   const navbar = document.querySelector('.navbar');
 
@@ -21,42 +29,89 @@ const Header = () => {
   //     }
   //   });
   // }, []);
+
   return (
     <>
-      <Navbar expand="md" className="navbar sticky-navbar">
+      <Navbar key='md' expand='md' data-bs-theme='dark' Navbar fixed='top'>
+        <Container fluid>
+          <Navbar.Brand href='#'>
+            <img src={ArmadaIcon} className='logo-blue d-inline-block align-top' alt='Logo_Armada' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='offcanvasNavbar-expand-md' className='text-light' />
+          <Navbar.Offcanvas id='offcanvasNavbar-expand-md' aria-labelledby='offcanvasNavbarLabel-expand-md' placement='end' data-bs-theme='dark'>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id='offcanvasNavbarLabel-expand-md'>Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className='justify-content-end flex-grow-1 pe-3'>
+                <Nav.Link href='#action1'>HOME</Nav.Link>
+                <Nav.Link href='#action2'>ABOUT US</Nav.Link>
+                <NavDropdown title='LAYANAN' id='offcanvasNavbarDropdown-expand-md'>
+                  <NavDropdown.Item href='#action3'>STORE</NavDropdown.Item>
+                  <NavDropdown.Item href='#action4'>SERVICE</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href='#action5'>SOFTWARE</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='#action6'>PRICELIST</Nav.Link>
+                <Nav.Link href='#action7'>TESTIMONIAL</Nav.Link>
+              </Nav>
+              <Button variant='outline-success'>Search</Button>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+      {/* <Navbar expand='md' className='navbar'>
         <Container fluid>
           <Navbar>
             <Container>
-              <Navbar.Brand href="#home">
+              <Navbar.Brand href='#home'>
                 <div>
-                  <img
-                    src={ArmadaIcon}
-                    className="logo-blue d-inline-block align-top"
-                    alt="Logo_Armada"
-                  />
+                  <img src={ArmadaIcon} className='logo-blue d-inline-block align-top' alt='Logo_Armada' />
                 </div>
               </Navbar.Brand>
             </Container>
           </Navbar>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">HOME</Nav.Link>
-              <Nav.Link href="#about">ABOUT US</Nav.Link>
-              <NavDropdown title="LAYANAN" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">STORE</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">SERVICE</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">SOFTWARE</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#pricelist">PRICELIST</Nav.Link>
-              <Nav.Link href="#testimonial">TESTIMONIAL</Nav.Link>
-            </Nav>
-            <Button className="ms-4 me-2 w-button" variant="outline-light">
-              GET APP
-            </Button>
-          </Navbar.Collapse>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' onClick={handleShow} />
+          <Offcanvas show={show} onHide={handleClose} {...props} placement='end'>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className='ms-auto'>
+                <Nav.Link href='#home'>
+                  <HouseFill />
+                  HOME
+                </Nav.Link>
+                <Nav.Link href='#about'>
+                  <FileEarmarkPersonFill />
+                  ABOUT US
+                </Nav.Link>
+                <NavDropdown title='LAYANAN' id='basic-nav-dropdown'>
+                  <NavDropdown.Item href='#action/3.1'>STORE</NavDropdown.Item>
+                  <NavDropdown.Item href='#action/3.2'>SERVICE</NavDropdown.Item>
+                  <NavDropdown.Item href='#action/3.3'>SOFTWARE</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='#pricelist'>PRICELIST</Nav.Link>
+                <Nav.Link href='#testimonial'>TESTIMONIAL</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Offcanvas>
+          <Nav className='ms-auto d-none d-md-block'>
+            <Nav.Link href='#home'>HOME</Nav.Link>
+            <Nav.Link href='#about'>ABOUT US</Nav.Link>
+            <NavDropdown title='LAYANAN' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>STORE</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>SERVICE</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>SOFTWARE</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href='#pricelist'>PRICELIST</Nav.Link>
+            <Nav.Link href='#testimonial'>TESTIMONIAL</Nav.Link>
+          </Nav>
+          <Button className='ms-4 me-2 w-button d-none d-md-block' variant='outline-light'>
+            GET APP
+          </Button>
         </Container>
-      </Navbar>
+      </Navbar> */}
     </>
   );
 };
