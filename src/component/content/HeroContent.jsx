22e9effdb7f01e6486e325d589f17ed3d5556b1b
4gungriyant0 <4gungriyant0@gmail.com>
2023-11-08 07:34:08 +0700

    layoutingV5

--- src/component/content/HeroContent.jsx
+++ src/component/content/HeroContent.jsx
@@ -1,18 +1,73 @@
+import {Container, Row, Col} from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import HPElite from "../../assets/img/Hp-Eliete.png";
+import CPUBlack from "../../assets/img/cpublack.png";
+import Damkar from "../../assets/img/damkar.png";
+import CrossPlatform from "../../assets/img/Cross-platform.png";
+
 const HeroContent = () => {
+  const hero = [
+    {
+      img: HPElite,
+      tag: "Udah muter muter ujungnya ke Armada",
+      desc: "Armadacom adalah Toko komputer terlengkap di Wonosobo, menyediakan berbagai perangkat komputer dan perangkat jaringan",
+    },
+    {
+      img: CPUBlack,
+      tag: "Ngapain Berhari hari Kalo Sehari Jadi",
+      desc: "Armadacom.id melayani service komputer, Laptop, Printer maupun Jaringan dan CCTV di wonosobo dengan kualitas pelayanan terbaik",
+    },
+    {
+      img: Damkar,
+      tag: "Anda Sibuk? Tidak bisa ke Toko Offline?",
+      desc: "Selain jualan dan servis, ARMADA juga punya layanan antar jemput Lo.. di DAMKARin aja, Kapan lagi bisa servis/beli tanpa keluar rumah",
+    },
+    {
+      img: CrossPlatform,
+      tag: "Bikin Aplikasi Juga Bisa Tentunya",
+      desc: "Bikin aplikasi berbasis web ataupun Android di Wonosobo ya ke Armadacom.id Sebagai penunjang aktifitas bisnis kalian",
+    },
+  ];
   return (
-    <div
-      id="home"
-      style={{
-        color: "#fff",
-        backgroundColor: "#2e64aa",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      HeroContent
-    </div>
+    <>
+      <Container id="home" className="slideshow" fluid>
+        <Carousel controls={false} indicators={true} fade>
+          {hero.map((hero, index) => (
+            <Carousel.Item key={index} className="carousel-item">
+              <Row sm={8} className="row-carousel d-flex justify-content-center justify-content-md-end align-items-center">
+                <Col sm={8} className="d-flex justify-content-center justify-content-md-center">
+                  <img src={hero.img} alt="slide" />
+                </Col>
+                <Col sm={8} className="caption pt-3">
+                  <h2 className="fw-bold text-uppercase fs-3" style={{textShadow: "rgb(85, 85, 85) 2px 2px 5px"}}>
+                    {hero.tag}
+                  </h2>
+                  <p className="desc pt-1">{hero.desc}</p>
+                </Col>
+              </Row>
+              <Row sm={8} className="justify-content-center">
+                <Col className="text-center">
+                  <Button
+                    className="mt-1 w-button"
+                    variant="light"
+                    href="#about"
+                    style={{
+                      color: "#2e64aa",
+                      fontWeight: "600",
+                      textShadow: "1px 1px 1px #fff",
+                      boxShadow: "rgb(63 63 63 / 50%) 0px 5px 10px 0px",
+                    }}
+                  >
+                    Selengkapnya
+                  </Button>
+                </Col>
+              </Row>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </Container>
+    </>
   );
 };
 
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,26 +1,12 @@
-import { Container, Row, Col } from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
+import {GeoAltFill, ClockHistory, EnvelopeFill, TelephoneInboundFill} from "react-bootstrap-icons";
 import "./footer.css";
-import Bintang from "./../../assets/brand/bintang.png";
-import Elena from "./../../assets/brand/elena.png";
-import Smk from "./../../assets/brand/smk_informatika_wsb.png";
-import Sumbera from "./../../assets/brand/sumber_agung.png";
-import Syf from "./../../assets/brand/syf.png";
 
 const Footer = () => {
   return (
-    <Container
-      id="footer"
-      style={{
-        backgroundColor: "#2e64aa",
-        height: "35vh",
-      }}
-      fluid
-    >
-      <Row
-        className="align-items-start justify-content-center text-light"
-        style={{ paddingTop: "2.3rem" }}
-      >
-        <Col xs={10} md={3} className="text-center text-md-start">
+    <Container id="footer" className="footer" fluid>
+      <Row className="align-items-start justify-content-center text-light" style={{paddingTop: "2.3rem", paddingBottom: "1rem"}}>
+        <Col xs={10} md={3} className="maps">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.2354472943107!2d109.89963278960028!3d-7.360422349200887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7aa1aaad2dd44d%3A0x6a66eadf4d7bc6e0!2sArmada%20Computer!5e0!3m2!1sen!2sid!4v1697420773581!5m2!1sen!2sid"
             // style={{ border: "1px solid" }}
@@ -28,76 +14,82 @@ const Footer = () => {
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             className="rounded"
-            style={{ width: "270px" }}
+            style={{width: "100%", boxShadow: "rgb(85, 85, 85) 2px 2px 5px"}}
           />
         </Col>
-        <Col xs={10} md={4} className="text-center">
-          <Col
-            md={12}
-            className="fw-bold"
-            style={{ textShadow: "rgb(85, 85, 85) 2px 2px 5px" }}
-          >
-            Partner Bussiness
+        <Col xs={10} md={3} className="address" style={{fontSize: "12px"}}>
+          <Col md={12} className="fw-bold" style={{fontSize: "14px", textShadow: "rgb(85, 85, 85) 2px 2px 5px"}}>
+            ARMADA COMPUTER
           </Col>
-          <Row className="justify-content-center p-md-3">
-            <Col md={4} xs={6}>
-              <img
-                src={Bintang}
-                alt="Bintang Sablon"
-                style={{ width: "150px", padding: "0.5rem" }}
-              />
+          <Row style={{lineHeight: "1.8rem", marginTop: "0.7rem"}}>
+            <Col md={12}>
+              <GeoAltFill />
+              Jln. Girimargo 10A, Kerkop, Wonosobo
             </Col>
-            <Col md={4} xs={6}>
-              <img
-                src={Elena}
-                alt="House of Elena"
-                style={{ width: "150px", padding: "0.5rem" }}
-              />
+            <Col md={12}>
+              <GeoAltFill />
+              Jam Operasional : Senin - Sabtu | 08.00 - 16.30
             </Col>
-            <Col md={4} xs={6}>
-              <img
-                src={Smk}
-                alt="SMK Informatika"
-                style={{ width: "150px", padding: "0.5rem" }}
-              />
+            <Col md={12}>
+              <GeoAltFill />
+              armadacom.wsb@gmail.com
             </Col>
-            <Col md={4} xs={6} className="pt-md-3">
-              <img
-                src={Sumbera}
-                alt="Sumber Agung"
-                style={{ width: "150px", padding: "0.5rem" }}
-              />
-            </Col>
-            <Col md={4} xs={6} className="pt-md-3">
-              <img
-                src={Syf}
-                alt="SYF Store"
-                style={{ width: "150px", padding: "0.5rem" }}
-              />
+            <Col md={12}>
+              <GeoAltFill />
+              (+62) 286 322949
             </Col>
           </Row>
         </Col>
-        <Col xs={10} md={3} className="text-center text-md-end">
-          <Col
-            md={12}
-            className="fw-bold"
-            style={{ textShadow: "rgb(85, 85, 85) 2px 2px 5px" }}
-          >
+        <Col xs={10} md={2} className="contact ms-3">
+          <Col md={12} className="fw-bold" style={{textShadow: "rgb(85, 85, 85) 2px 2px 5px"}}>
             Contact Us
           </Col>
-          <Row className="flex-column py-md-3">
-            <Col>Facebook</Col>
-            <Col className="py-1">Instagram</Col>
-            <Col>Tiktok</Col>
-            <Col className="py-1">Whatsapp</Col>
+          <Row className="flex-column py-md-2">
+            <Col>
+              <GeoAltFill />
+              Facebook
+            </Col>
+            <Col className="py-1">
+              <GeoAltFill />
+              Instagram
+            </Col>
+            <Col>
+              <GeoAltFill />
+              Tiktok
+            </Col>
+            <Col className="py-1">
+              <GeoAltFill />
+              Whatsapp
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={10} md={2} className="contact ms-3">
+          <Col md={12} className="fw-bold" style={{textShadow: "rgb(85, 85, 85) 2px 2px 5px"}}>
+            We Ready On
+          </Col>
+          <Row className="flex-column py-md-2">
+            <Col>
+              <GeoAltFill />
+              Bukalapak
+            </Col>
+            <Col className="py-1">
+              <GeoAltFill />
+              Shopee
+            </Col>
+            <Col>
+              <GeoAltFill />
+              SIPLah
+            </Col>
+            <Col className="py-1">
+              <GeoAltFill />
+              Tokopedia
+            </Col>
           </Row>
         </Col>
       </Row>
-      <Row className="align-items-center justify-content-center text-light">
-        <Col md={10} className="text-light text-center">
-          <p style={{ fontSize: "10px" }}>
-            All Rights Reserved. Armadacom.id © 2023 - Tentackle Studio
-          </p>
+      <Row md={12} className="text-light">
+        <Col md={12} className="text-center" style={{fontSize: "10px"}}>
+          All Rights Reserved. Armadacom.id © 2023 - Tentackle Studio{" "}
         </Col>
       </Row>
     </Container>
